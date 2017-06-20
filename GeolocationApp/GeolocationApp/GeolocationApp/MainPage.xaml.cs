c55981f578fe81d3c7a9348596cf11093d489128
Maximiliano Villa <max_210291@hotmail.com>
2017-06-20 05:56:33 -0500

    Code5

--- GeolocationApp/GeolocationApp/GeolocationApp/MainPage.xaml.cs
+++ GeolocationApp/GeolocationApp/GeolocationApp/MainPage.xaml.cs
@@ -16,49 +16,6 @@ namespace GeolocationApp
         public MainPage()
         {
             InitializeComponent();
-            GetPosition();
-        }
-
-        private async void GetPosition()
-        {
-            try
-            {
-                var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 50;
-
-                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
-                if (position == null)
-                    return;
-
-
-                var gn=new GeoNames(position.Latitude,position.Longitude);
-               var result=  await   gn.CountryCode();
-
-                var res = await gn.CountryInfo("MX");
-
-
-                // GeoNames(position.Latitude,position.Longitude);
-
-                //Console.WriteLine("Position Status: {0}", position.Timestamp);
-                //Console.WriteLine("Position Latitude: {0}", position.Latitude);
-                //Console.WriteLine("Position Longitude: {0}", position.Longitude);
-            }
-            catch (Exception ex)
-            {
-                string error = ex.Message;
-                Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
-            }
-        }
-
-        private async void GeoNames(double latitude,double longitude)
-        {
-            HttpClient client;
-            string url = $"http://api.geonames.org/countryCode?lat={latitude}&lng={longitude}&username=maxvilla21";
-
-            //var response
-
-
-            //http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=demo
-        }
+        }    
     }
 }
